46270c07ec3d1cb5ab7fbde254e140aec529c067
Gabriel Masson <g.masson010@gmail.com>
2024-09-18 18:25:41 -0300

    test: add update product use case test

---
+++ src/modules/products/application/update-product/__tests__/update-product.use-case.spec.ts
@@ -0,0 +1,29 @@
+import { createSpyObj } from 'jest-createspyobj';
+
+import { ProductsRepository } from '../../ports/products.repository';
+import { ProductFixtures } from '../../../__tests__/fixtures/product.fixtures';
+import { ProductEntity } from '~/modules/products/domain/entities/product.entity';
+import { UpdateProductUseCase } from '../update-product.use-case';
+
+describe('DeleteProduct UseCase', () => {
+  const productsRepository = createSpyObj(ProductsRepository, ['update']);
+
+  it('should delete a product', async () => {
+    const product = ProductFixtures.simpleProduct();
+
+    const newBrands = 'Nike';
+    const updatedProduct: ProductEntity = { ...product, brands: newBrands };
+    productsRepository.update.mockResolvedValueOnce(updatedProduct);
+
+    const sut = new UpdateProductUseCase(productsRepository);
+
+    const result = await sut.execute(product.code, updatedProduct);
+
+    expect(productsRepository.update).toHaveBeenCalledWith(
+      product.code,
+      updatedProduct,
+    );
+    expect(result).toEqual(updatedProduct);
+    expect(result.brands).toBe(newBrands);
+  });
+});
